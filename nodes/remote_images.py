43cdeea4c77aea61c31b8a61e6c1e884f0b8e13f
gene <1228729672@qq.com>
2023-12-30 22:54:22 +0800

    Update remote_images.py

--- nodes/remote_images.py
+++ nodes/remote_images.py
@@ -104,9 +104,7 @@ class LoadImageUrl:
 	CATEGORY = "remote"
 
 	def load_image_url(self, face_mask,body_mask):
-		print(f'face_mask:{face_mask}')
-		print(f'body_mask:{body_mask}')
-		expanded_mask = pil_to_tensor(face_mask)
+		expanded_mask = cv2.imdecode(face_mask,cv2.IMREAD_GRAYSCALE)
 		_, threshold = cv2.threshold(expanded_mask, 128, 255, cv2.THRESH_BINARY)
 		qualified_Zuobiao, center_x, center_y = Direction_face_ZuoBiao(threshold)
 		# 参数定义
@@ -114,7 +112,8 @@ class LoadImageUrl:
 		Vertical_num = 200
 		expanded_mask, contours = Borad_draw(threshold, qualified_Zuobiao, Horizon_num, Vertical_num, center_x, center_y)
 		expanded_mask_copy = Borad_PengZhang(expanded_mask, contours)
-		body=cv2.threshold(pil_to_tensor(body_mask), 128, 255, cv2.THRESH_BINARY)
+		bodymask=cv2.imdecode(body_mask,cv2.IMREAD_GRAYSCALE)
+		body=cv2.threshold(bodymask, 128, 255, cv2.THRESH_BINARY)
 		width = body.shape[0]; height = body.shape[1]
 		im1_copy = cv2.resize(expanded_mask_copy, (height, width))
 		img_face_expect_body = cv2.multiply(im1_copy, body)
