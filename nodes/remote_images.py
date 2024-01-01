0b2d79e95f16af719f5a6addd0755ccc6280747d
gene <1228729672@qq.com>
2024-01-01 15:04:59 +0800

    Update remote_images.py

--- nodes/remote_images.py
+++ nodes/remote_images.py
@@ -134,9 +134,8 @@ class LoadImageUrl:
 		img_face_expect_body = cv2.multiply(im1_copy, body)
 		reult = cv2.cvtColor(img_face_expect_body, cv2.COLOR_BGR2RGB)
 		torch_img = torch.from_numpy(reult)
-
-		torch_img=torch_img.permute(2, 0, 1)
 		torch_img = torch_img.float() / 255.0
+		torch_img=torch_img.permute(2, 0, 1)
 		return (torch_img,)
 
 
