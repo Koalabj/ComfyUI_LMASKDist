9bb5c7bf3be14f9d8800e7d5469ff5198150bdfb
gene <1228729672@qq.com>
2024-01-02 17:19:30 +0800

    Update remote_images.py

--- nodes/remote_images.py
+++ nodes/remote_images.py
@@ -287,7 +287,9 @@ class BodyMask:
 		else:
 			print('未检测到物体，固未填充')
 		
-		pil_image = Image.fromarray(np.uint8(max_y_coordinate_face[0]))
+
+
+		pil_image = Image.fromarray(np.uint8(face_img_mask[0]))
 		torch_img=pil_to_tensor_grayscale(pil_image)
 
 		return (torch_img,)
