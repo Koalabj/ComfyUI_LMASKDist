16bd76fd956282a8713f046c19547f04b221c964
gene <1228729672@qq.com>
2024-01-02 11:18:26 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,12 +6,14 @@ NODE_DISPLAY_NAME_MAPPINGS = {}
 def remote_images():
 	global NODE_CLASS_MAPPINGS
 	global NODE_DISPLAY_NAME_MAPPINGS
-	from .nodes.remote_images import LoadImageUrl
+	from .nodes.remote_images import LoadImageUrl,BodyMask
 	NODE_CLASS_MAPPINGS.update({
 		"LMASKImage": LoadImageUrl,
+		"BodyMask":BodyMask
 	})
 	NODE_DISPLAY_NAME_MAPPINGS.update({
 		"LMASKImage": "LMASKImage",
+		"BodyMask":BodyMask
 	})
 
 
