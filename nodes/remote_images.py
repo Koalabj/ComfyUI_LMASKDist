3e528aa0a8c58d81998b4c5032012dba3e3eb0c9
gene <1228729672@qq.com>
2024-01-06 19:54:05 +0800

    Update remote_images.py

--- nodes/remote_images.py
+++ nodes/remote_images.py
@@ -29,7 +29,7 @@ def create_smooth_bezier_polygon(mask):
         center_y = y + h // 2 + vertical_shift
 
         # 设置圆的半径
-        radius = int(original_radius * random.uniform(2, 2.5))
+        radius = int(original_radius * random.uniform(1.5,2))
 
         # 调整圆的位置和大小，确保它在图像范围内
         center_x = max(radius, min(mask.shape[1] - radius, center_x))
