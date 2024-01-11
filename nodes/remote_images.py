589e50c0e68800844198aef10e30a33088a50efb
gene <1228729672@qq.com>
2024-01-11 17:58:01 +0800

    Update remote_images.py

--- nodes/remote_images.py
+++ nodes/remote_images.py
@@ -380,14 +380,15 @@ class BodyMask:
 		#获取衣服蒙版的定坐标
 		top=getMaskTop(body)
 		print(f"顶部坐标{top}")
-		person_img=cv2.imread(path)
-		person_img = cv2.cvtColor(person_img, cv2.COLOR_BGR2GRAY)
+		person=cv2.imread(path)
+		person_img = cv2.cvtColor(person, cv2.COLOR_BGR2GRAY)
 		_, numpy_image = cv2.threshold(person_img, 127, 255, cv2.THRESH_BINARY)
 		kernel = np.ones((3,3),np.uint8) 
 		numpy_image = cv2.erode(numpy_image, kernel, iterations=1)
 		person_img_mask = cv2.dilate(numpy_image, kernel, iterations=1)
 		path="/root/autodl-tmp/ComfyUI/input/yt2.png"
 		cv2.imwrite(path,person_img_mask)
+		person_img_mask=person
 		# 获取输入的图片
 		pic=tensor_to_pil(image)
 		path="/root/autodl-tmp/ComfyUI/input/yt.png"
