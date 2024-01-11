a91c972dd2151308604c305f196bcaffe33e2ff5
gene <1228729672@qq.com>
2024-01-12 00:02:35 +0800

    Update remote_images.py

--- nodes/remote_images.py
+++ nodes/remote_images.py
@@ -539,11 +539,25 @@ class BodyMask:
 		final=blacken_above_y(final_img1,int(bootm))
 		# 反色处理
 		inverted_mask = cv2.bitwise_not(final)
-		white_image = np.ones_like(inverted_mask) * 255
-		black_mask = np.all(inverted_mask == [0, 0, 0], axis=-1)
-		inverted_mask[~black_mask] = white_image[~black_mask]
+            
+		result_image = inverted_mask.copy()
 
-		result = cv2.cvtColor(inverted_mask, cv2.COLOR_BGR2RGB)
+		# 创建一个黑色掩膜，标记出所有非黑色的像素
+		black_mask = np.all(inverted_mask == [0, 0, 0], axis=-1)
+
+		# 创建一个白色掩膜，标记出所有非白色的像素
+		white_mask = np.all(inverted_mask == [255, 255, 255], axis=-1)
+
+		# 标记出所有既非黑色也非白色的像素
+		non_black_white_mask = ~(black_mask | white_mask)
+
+		# 将非黑非白的像素替换为白色
+		result_image[non_black_white_mask] = [255, 255, 255]
+
+
+
+
+		result = cv2.cvtColor(result_image, cv2.COLOR_BGR2RGB)
 		pil_image = Image.fromarray(result)
 		torch_img=pil_to_tensor_grayscale(pil_image)
         # # 反色处理
