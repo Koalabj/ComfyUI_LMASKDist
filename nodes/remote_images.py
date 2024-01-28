a1a8a20e1b9aaf39c54d7e486782c17c39889f9f
gene <1228729672@qq.com>
2024-01-28 22:12:59 +0800

    Update remote_images.py

--- nodes/remote_images.py
+++ nodes/remote_images.py
@@ -587,6 +587,7 @@ class MaskLoadUrl:
 		with requests.get(url, stream=True) as r:
 			r.raise_for_status()
 			i = Image.open(r.raw)
+		print(f"图片格式{i.shape}")
 		image = i.convert("RGBA")
 		image = np.array(image).astype(np.float32) / 255.0
 		image = torch.from_numpy(image)[None,]
